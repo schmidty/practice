ef8e63eb9425966dcd11fb47a1a0bb903b6fbef3
Matthew Schmidt <schmidty99203@gmail.com>
2017-05-09 19:12:04 -0700

    removed migrations php file

--- migrations-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-        'dbname'   => 'practice',
-        'user'     => 'root',
-        'password' => 'password',
-        'host'     => 'localhost',
-        'driver'   => 'pdo_mysql',
-);
-//return [ file_get_contents('config.yml')['doctrine']['dbal'] ];
